0130ef74d1278946b3c282f1d3a2e9df622e7f29
Mathias <mathiasjtaylor@gmail.com>
2020-11-11 10:45:33 -0800

    Added API routes

--- backend/src/main/java/com/cognixia/jump/repository/TransactionRepository.java
+++ backend/src/main/java/com/cognixia/jump/repository/TransactionRepository.java
@@ -3,9 +3,9 @@ package com.cognixia.jump.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cognixia.jump.model.Product;
+import com.cognixia.jump.model.Transaction;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Product, Integer> {
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
 }
