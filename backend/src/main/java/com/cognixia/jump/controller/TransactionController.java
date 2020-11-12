3f29c9bfdd210b417f9b1441182e29f68072f28c
natashang <50751197+natashang@users.noreply.github.com>
2020-11-11 18:06:10 -0800

    added CrossOrigin annotation to rest controllers

--- backend/src/main/java/com/cognixia/jump/controller/TransactionController.java
+++ backend/src/main/java/com/cognixia/jump/controller/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cognixia.jump.exceptions.ResourceAlreadyExistsException;
 import com.cognixia.jump.model.Transaction;
@@ -13,6 +14,7 @@ import com.cognixia.jump.repository.TransactionRepository;
 
 @RequestMapping("/api")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TransactionController {
 
 	@Autowired
