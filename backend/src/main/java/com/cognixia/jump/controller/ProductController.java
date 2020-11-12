ade8b4806adde8712d2af5a0b2b48a5adfb782f6
Mathias <mathiasjtaylor@gmail.com>
2020-11-12 10:53:31 -0800

    Added delete and update route to product

--- backend/src/main/java/com/cognixia/jump/controller/ProductController.java
+++ backend/src/main/java/com/cognixia/jump/controller/ProductController.java
@@ -1,15 +1,20 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
 import com.cognixia.jump.model.Product;
 import com.cognixia.jump.repository.ProductRepository;
@@ -40,6 +45,35 @@ public class ProductController {
 		Product newProduct = repository.save(product);
 
 		return newProduct;
+	}
+
+	@PutMapping("/update/product")
+	public @ResponseBody String updateProduct(@RequestBody Product product) {
+
+		Optional<Product> found = repository.findById(product.getId());
+
+		if (found.isPresent()) {
+			repository.save(product);
+			return "Saved: " + product.toString();
+		} else {
+			return "Could not update product";
+		}
+
+	}
+
+	@DeleteMapping("/delete/product/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+
+		Optional<Product> found = repository.findById(id);
+
+		if (found.isPresent()) {
+			repository.deleteById(id);
+
+			return ResponseEntity.status(200).body("Product has been deleted");
+
+		} else {
+			return ResponseEntity.status(400).header("Product id", id + "").body("Product not found");
+		}
 	}
 
 }
