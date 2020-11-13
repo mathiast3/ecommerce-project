f0fc47eea037955d535e16efad44e4485174cb0c
KDuncan312 <ace08873@gmail.com>
2020-11-13 03:41:16 -0500

    adding load

---
+++ frontend/src/components/shared/styles/Loader.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Loaders = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  h1 {
+    font-weight: bold;
+  }
+`;
